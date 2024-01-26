a929745cff989ddc264d98f7a4dd937511ec483b
amraibra <92942609+amraibra@users.noreply.github.com>
2024-01-26 12:19:28 -0500

    Landing page basic

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,34 +1,79 @@
 <template>
-  <ion-header :translucent="true">
-    <ion-toolbar>
-      <div class="flex justify-between items-center w-full h-12 px-5">
-        <!-- Logo and title -->
-        <div class="flex items-center pl-5">
-          <img src="/src/output-onlinepngtools-2.png" alt="CodeExhibit Logo" class="h-10" />
-          <ion-title class="text-codeExhibit font-londrina text-3xl normal-case font-normal leading-normal -m-4">CodeExhibit</ion-title>
+  <ion-page class="bg-custom-blue h-full overflow-hidden">
+      <header class="bg-custom-blue text-white">
+        <!--Navbar-->
+        <nav class="container mx-auto py-4 flex justify-between items-center">
+          <!--Logo + Title-->
+          <div class="flex items-center">
+            <span class="font-extrabold text-lg tracking-tight text-custom-orange font-mono">CodeExhibit</span>
+          </div>
+
+          <!--Navigation buttons-->
+          <div class="hidden md:flex items-center space-x-1">
+            <a href="/home" class="py-2 px-3 text-white hover:bg-custom-orange rounded-full 
+            transition duration-100 font-mono font-semibold text-sm">Home</a>
+            <a href="/info" class="py-2 px-3 text-white hover:bg-custom-orange rounded-full 
+            transition duration-100 font-mono font-semibold text-sm">About Us</a>
+            <a href="#" class="py-2 px-3 text-white hover:bg-custom-orange rounded-full 
+            transition duration-100 font-mono font-semibold text-sm">Contact</a>
+            <a href="/projects" class="py-2 px-3 text-white bg-custom-orange rounded-full 
+            transition duration-100 font-mono font-semibold text-sm">Projects</a>
+          </div>
+        </nav>
+      </header>
+      <ion-content class="bg-custom-blue overflow-hidden">
+        <div class="bg-custom-blue text-white h-60 pt-10 pl-16 items-center">
+          <h1 class="text-3xl md:text-4xl font-bold text-custom-orange mb-3">
+            Project Today,
+          </h1>
+          <h2 class="text-3xl md:text-4xl font-bold mb-3">
+            Portfolio Tomorrow:
+          </h2>
+          <h2 class="text-3xl md:text-4xl font-bold">
+            Where Students Shine.
+          </h2>
         </div>
+        <div class="bg-stone-300 text-custom-orange h-80">
 
-        <!-- Navigation buttons -->
-        <div class="flex gap-2">
-          <button class="text-white text-center font-semibold text-sm bg-transparent">Home</button>
-          <button class="text-white text-center font-semibold text-sm bg-transparent">About</button>
-          <button class="text-white text-center font-semibold text-sm bg-transparent">Contact</button>
         </div>
-
-        <!-- Projects button -->
-        <button class="bg-codeExhibit px-6 py-2 text-white rounded-full">Projects</button>
-      </div>
-    </ion-toolbar>
-  </ion-header>
-
-  <ion-content :fullscreen="true" class="content-style">
-    <img src="/src/Rectangle1.svg" alt="Rectangle1" class="w-full h-auto mx-auto -z-1" />
-  </ion-content>
+        <div class="bg-custom-blue text-white h-1/5">
+          <div class="container mx-auto">
+          </div>
+        </div>
+      </ion-content>
+  </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 </script>
 
 <style scoped>
+#container {
+  text-align: center;
+  
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  
+  color: #8c8c8c;
+  
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,35 @@ export default {
         londrina: ['Londrina Solid', 'sans-serif'], // Existing custom font
         inter: ['Inter', 'sans-serif'], // Added Inter font
       },
+        'custom-blue' : '#264653',
+        'custom-teal' : '#2a9d8f',
+        'custom-yellow' : '#e9c46a',
+        'custom-orange-light' : '#f4a261',
+        'custom-orange' : '#e76f51',
+        'custom-gray' : '#d9d9d9'
+      },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }
+        }
+      },
+      animation: {
+        typing: "typing 2s steps(20) infinite alternate, blink .7s infinite"
+      }
     },
   },
   plugins: [],
